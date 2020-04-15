5ae75a349f12eace1b91f04c9d1c4529d17fbeb0
Matt Thomas <matt.thomas@sproutsocial.com>
2020-04-15 14:45:02 -0500

    Add axios and make a get request to the api on load

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import axios from 'axios';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    @track orgs = '';
+
+    connectedCallback() {
+        axios
+            .get('/api/v1/orgs') // http://localhost:3001/api/v1/orgs
+            .then((result) => {
+                this.orgs = result.data;
+            });
+    }
+}
