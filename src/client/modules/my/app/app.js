cd49c5e4b19c43120719662e0dc962e60b3e743c
Felix Markman <felix.markman@sproutsocial.com>
2020-04-16 16:16:11 -0500

    add axios method for user get request in App

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -3,12 +3,19 @@ import axios from 'axios';
 
 export default class App extends LightningElement {
     @track orgs = '';
+    @track user = '';
 
     connectedCallback() {
         axios
             .get('/api/v1/orgs') // http://localhost:3001/api/v1/orgs
             .then((result) => {
                 this.orgs = result.data;
+            });
+
+        axios
+            .get('/api/v1/user') // http://localhost:3001/api/v1/user
+            .then((result) => {
+                this.user = result.data;
             });
     }
 }
